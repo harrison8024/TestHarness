58a385abe0bc9ba7fc566f1d7cbe107ea06a0ff3
Fang Wang <fwang15@syr.edu>
2021-11-09 11:23:20 -0800

    create dll_maker for making the testable DLL files

---
+++ dll_maker/Dll1/Dll1/Dll1.h
@@ -0,0 +1,23 @@
+//Dll1.h  Declaration of Dll1
+//Fang Wang, Syracuse University
+
+
+#ifdef DLL1_EXPORTS
+#define DLL1_API __declspec(dllexport)
+#else
+#define DLL1_API __declspec(dllimport)
+#endif
+
+#include "Itest.h"
+
+class TestClass1 : public ITest {
+
+public:
+	bool test() override;
+};
+
+
+extern "C" DLL1_API TestClass1 * createTest() {
+	return new TestClass1;
+};
+
---
+++ dll_maker/Dll1/Dll1/ITest.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef ITEST_H
+#define ITEST_H
+
+#ifdef TEST_EXPORTS
+#define TEST_API __declspec(dllexport)
+#else
+#define TEST_API __declspec(dllimport)
+#endif
+
+class  ITest
+{
+public:
+	virtual ~ITest() {};
+	virtual bool test() = 0;
+};
+#endif
