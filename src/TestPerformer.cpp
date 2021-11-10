1ebfe369e139e66970dcac2f8867b4c10396279d
anguye26 <92067647+anguye26@users.noreply.github.com>
2021-11-09 20:49:28 -0500

    Update TestPerformer.cpp

--- src/TestPerformer.cpp
+++ src/TestPerformer.cpp
@@ -1,1 +1,71 @@
 #include "TestPerformer.h"
+<<<<<<< Updated upstream
+=======
+#include "TestManager.h"
+#include "testfile.h"
+#include "Logger.h"
+#include "Testable.h"
+#include "TestObjects.h"
+
+int main() {
+	// Initialize Classes
+	TestManager testManager(Logger::LOW);
+
+	// Load Testables
+	Testable t1("Test True", TestTrue);
+	Testable t2("Test False", TestFalse);
+	Testable t3("Test Bad Allocation", TestBadAlloc);
+	Testable t4("Test Bad Cast", TestBadCast);
+	Testable t5("Test Bad Type ID", TestBadTypeID);
+	Testable t6("Test Bad Weak Pointer", TestBadWeakPtr);
+	Testable t7("Test Bad Function Call", TestBadFunctionCall);
+	Testable t8("Test Bad Exception", TestBadException);
+	Testable t9("Test Add", TestAdd);
+	Testable t10("Test Allocation", TestAllocate);
+	Testable t11("Test Divide Zero", TestDivide);
+
+	// logLevel = LOW
+	testManager.performSingleTest(t1);
+	testManager.performSingleTest(t2);
+	testManager.performSingleTest(t3);
+	testManager.performSingleTest(t4);
+	testManager.performSingleTest(t5);
+	testManager.performSingleTest(t6);
+	testManager.performSingleTest(t7);
+	testManager.performSingleTest(t8);
+	testManager.performSingleTest(t9);
+	testManager.performSingleTest(t10);
+
+
+	// logLevel = MEDIUM
+	testManager.logger.setLogLevel(Logger::MEDIUM);
+
+	testManager.performSingleTest(t1);
+	testManager.performSingleTest(t2);
+	testManager.performSingleTest(t3);
+	testManager.performSingleTest(t4);
+	testManager.performSingleTest(t5);
+	testManager.performSingleTest(t6);
+	testManager.performSingleTest(t7);
+	testManager.performSingleTest(t8);
+	testManager.performSingleTest(t9);
+	testManager.performSingleTest(t10);
+
+	// logLevel = HIGH
+	testManager.logger.setLogLevel(Logger::HIGH);
+
+	testManager.performSingleTest(t1);
+	testManager.performSingleTest(t2);
+	testManager.performSingleTest(t3);
+	testManager.performSingleTest(t4);
+	testManager.performSingleTest(t5);
+	testManager.performSingleTest(t6);
+	testManager.performSingleTest(t7);
+	testManager.performSingleTest(t8);
+	testManager.performSingleTest(t9);
+	testManager.performSingleTest(t10);
+
+	std::cout << testManager.logger.getReport();
+
+}
+>>>>>>> Stashed changes
